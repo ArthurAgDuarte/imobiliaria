2bf5977b6c1ee6bc88120f30e84b147e5ccc8575
Arthur Duarte <duartearthur30@gmail.com>
2024-10-15 15:52:00 -0300

    estrutura basica

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// alert("Buenas")
+
+const listaImoveis = [
+    {
+        id: 1,
+        titulo: 'Apart. 3 dorm. Papicu',
+        descricao:'Lindo apartamento de 3 dormitórios localozado em um dos melhores',
+        valor: 'R$ 500.000,00',
+        area:'90 m²',
+        quartos: 3,
+        tipo: 'apartamento',
+        localização: 'Santos Dummont, Papicu',
+        venda_aluguel: 'venda', //alugel
+        finalidade: residencial, //comercial
+        fotos: [
+            'link1',
+            'link2',
+            'link3',
+            'link4',
+            'link5'
+        ]
+    }
+]
